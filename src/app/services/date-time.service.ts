82378e7d5e0fd8cc4fbe4f2f1496e7e222c0f518
Asraf Uddin Ahmed <asraf@dginnovationlab.com>
2019-02-07 10:53:56 +0600

    Date time related functions added

--- src/app/services/date-time.service.ts
+++ src/app/services/date-time.service.ts
@@ -15,4 +15,30 @@ export class DateTimeService {
   getDateFromJson(jsonDate: number) {
     return new moment(jsonDate).toDate();
   }
+  increaseDay(date: Date, numberOfDay: number): Date {
+    date.setDate(date.getDate() + numberOfDay);
+    return date;
+  }
+  decreaseDay(date: Date, numberOfDay: number): Date {
+    date.setDate(date.getDate() - numberOfDay);
+    return date;
+  }
+  getStartTimeOfToday(): Date {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+    return date;
+  }
+  getStartTimeOfTomorrow(): Date {
+    let date = new Date();
+    date = this.increaseDay(date, 1);
+    date.setHours(0, 0, 0, 0);
+    return date;
+  }
+  getTimezoneOffsetInMinute(): number {
+    return new Date().getTimezoneOffset();
+  }
+  scaleDateWithTimezoneOffset(date: Date): Date {
+    date.setMinutes(-this.getTimezoneOffsetInMinute());
+    return date;
+  }
 }
